87b67d9d60a4202cc2a5394d5787a31de2d0ef64
pvonderhaar <a.vonderhaar01@stud.uni-goettingen.de>
2024-05-17 14:25:01 +0200

    Created a new style for bot template

--- Chatbot/web_chatbot.py
+++ Chatbot/web_chatbot.py
@@ -7,18 +7,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    # Get context
     context = get_context()
     return render_template("chat_temp.html", context=context)
 
 
 @app.route("/ask", methods=["POST"])
 def ask():
-    # Get user input
     user_input = request.form["user_input"]
 
     context = get_context()
-    # Generate answer
+
     answer = generate_answer(context, user_input)
 
     return answer
