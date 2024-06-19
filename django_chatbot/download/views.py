f2237858e459e4f2323ddd9a82fe9f01d363fab2
a.vonderhaar01 <a.vonderhaar01@stud.uni-goettingen.de>
2024-06-19 16:55:45 +0200

    started developing download page

--- django_chatbot/download/views.py
+++ django_chatbot/download/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the download index.")
+    return render(request, 'download/download_temp.html')
+
+def select_platform():
+    return
