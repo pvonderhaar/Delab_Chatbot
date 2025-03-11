239f9488df29f5c0a3062efd366df5a3bd211744
pvonderhaar <pauline.vdh.01@gmail.com>
2025-03-11 17:40:23 +0100

    Genrating llm answer in web interface

--- django_chatbot/chatbot/utils.py
+++ django_chatbot/chatbot/utils.py
@@ -7,39 +7,27 @@ from pathlib import Path
 
 
 def generate_answer(user_input, context=None):
-    # TODO: load answers from docker
-    """    bot_answer = 'This will be the answer of the bot'
-    # I displayed the context here to check if the right context is given to this function
-    answer = f"Context: {context} <br> Your answer: {user_input} <br> {bot_answer}"
-    """
-    base_url = "http://localhost:8840/analytics"
+    base_url = "http://localhost:8840/"
     headers = {"Content-Type": "application/json"}
-    # TODO: Structure payload correctly, so that the api can use it
-    payload = {"texts": [user_input]}
 
-    # Send requests to different analytics endpoints
+    texts = []
+    # format context like the api requests
+    for entry in context.values():
+        for text in entry.values():
+            texts.append(text)
+    texts.append(user_input)
+    payload = {"texts": texts}
+    print("calc response")
     try:
+        response_llm = requests.post(f"{base_url}llm", headers=headers, json=payload).json()
+        #response_inference = requests.post(f"{base_url}inference", headers=headers, json=payload).json()
 
-        response_general = requests.post(base_url, headers=headers, json=payload).json()
-        response_sentiment = requests.post(f"{base_url}?analytics=sentiment", headers=headers, json=payload).json()
-        response_justification = requests.post(f"{base_url}?analytics=justification", headers=headers,
-                                               json=payload).json()
-        response_cosine = requests.post(f"{base_url}?analytics=cosine", headers=headers, json=payload).json()
-
-        # Combine the responses into a structured format
-        print("Calculating bot anwer")
-        bot_answer = {
-            "general": response_general,
-            "sentiment": response_sentiment,
-            "justification": response_justification,
-            "cosine": response_cosine
-        }
+        bot_answer = response_llm["df"][0]["out_llm"]
 
     except requests.exceptions.RequestException as e:
         bot_answer = f"Error connecting to Docker service: {e}"
 
-    # Display context and response
-    answer = f"Context: {context} <br> Your input: {user_input} <br> Bot answer: {json.dumps(bot_answer, indent=2)}"
+    answer = bot_answer
     return answer
 
 
@@ -77,8 +65,8 @@ def get_context(conv_df=None):
         return get_context(conv_df)
     if len(context_sequence)>=5:
         if (int(conv_seqid) > 3) & (int(conv_seqid)<=len(context_sequence)-2):
-            context_sequence = context_sequence[(context_sequence['conv_seqid']>=conv_seqid - 2) &
-                                                (context_sequence['conv_seqid']<=conv_seqid + 2)]
+            context_sequence = context_sequence[(context_sequence['conv_seqid'].astype(int)>=int(conv_seqid) - 2) &
+                                                (context_sequence['conv_seqid'].astype(int)<=int(conv_seqid) + 2)]
         else:
             # der Einfachkeit halber den context ab dem ersten post
             context_sequence = context_sequence.head(5)
